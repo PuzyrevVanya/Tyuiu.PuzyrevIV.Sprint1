1d18bff452c4bfb820ea6cf319e22a045dca2ed7
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-09-29 01:39:07 +0500

    Task0

---
+++ Tyuiu.PuzyrevIV.Sprint1.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PuzyrevIV.Sprint1.Task0.V7.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint1.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(5, res);
+        }
+    }
+}
+
---
+++ Tyuiu.PuzyrevIV.Sprint1.Task0.V7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PuzyrevIV.Sprint1.Task0.V7.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint1.Task0.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Пузырёв И. В. | СМАРТб 25-1";
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("* Спринт #1                                          *");
+            Console.WriteLine("* Тема: Базовые навыки в работы в C#                 *");
+            Console.WriteLine("* Задание #0                                         *");
+            Console.WriteLine("* Вариант #7                                         *");
+            Console.WriteLine("* Выполнил: Пузырёв Иван Вадимович СМАРТб 25-1       *");
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                           *");
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение    *");
+            Console.WriteLine("* 18 / 6 * 2 - (4 - 3) и печатает результат на экране*");
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                   *");
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("* 18 / 6 * 2 - (4 - 3)                               *");
+            Console.WriteLine("******************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                          *");
+            Console.WriteLine("******************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
