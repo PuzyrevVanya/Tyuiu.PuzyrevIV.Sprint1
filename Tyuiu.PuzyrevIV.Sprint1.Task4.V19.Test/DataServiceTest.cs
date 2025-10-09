26eed3ae3a22def2b0f3b5bacf3dcfa449f0b318
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-10-09 16:35:56 +0500

    Task4

---
+++ Tyuiu.PuzyrevIV.Sprint1.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+using Tyuiu.PuzyrevIV.Sprint1.Task4.V19.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint1.Task4.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            double wait = 1.5;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
