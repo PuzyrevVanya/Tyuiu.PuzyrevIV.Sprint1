eeb3495d726b08b8b0f2894304c7bfc8001428cf
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-10-09 17:19:47 +0500

    Task4

--- Tyuiu.PuzyrevIV.Sprint1.Task4.V19.Test/DataServiceTest.cs
+++ Tyuiu.PuzyrevIV.Sprint1.Task4.V19.Test/DataServiceTest.cs
@@ -13,9 +13,9 @@ namespace Tyuiu.PuzyrevIV.Sprint1.Task4.V19.Test
         public void ValidExpression()
         {
             DataService ds = new DataService();
-            double x = 52;
+            double x = 5;
             double y = 5;
-            double wait = 50.09615384615385;
+            double wait = 4;
             var res = ds.Calculate(x, y);
             Assert.AreEqual(wait, res);
         }
